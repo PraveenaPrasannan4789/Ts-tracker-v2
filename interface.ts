623c0fb5699b7d6b077b539a5c1e1e33f3e9662b
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-06-22 22:20:39 +0100

    extended interface added

--- interface.ts
+++ interface.ts
@@ -21,3 +21,16 @@ interface employee2 {
   readonly name?: string;
   age: number;
 }
+
+//Extending interface
+interface Person1 {
+  name: string;
+}
+
+interface Employee extends Person1 {
+  id: number;
+}
+const emp: Employee = {
+  name: "Praveena",
+  id: 1,
+}; //Combines properties (like intersection)
